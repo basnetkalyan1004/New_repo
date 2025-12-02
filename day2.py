ab1411150827d1f59a48d68b3dc0106b4eab96ca
kalyanbasnet <kalyanbasnet001@gmail.com>
2025-12-02 09:30:32 +0545

    initial projects

---
+++ day2.py
@@ -0,0 +1,8 @@
+# this is the practice day 3
+a = input("enter your first numer..")
+b =input("enter second number....")
+c = int(a) + int(b)
+print ("The sum of your number is ",c)
+print("Both number share the same value", a==b)
+print(a>b and b>=a)
+
---
+++ day3.py
@@ -0,0 +1,6 @@
+user="kalyan"
+pass="broadway"
+if (user==pass):
+    print("welcome boss")
+else:
+    print("Well Remember again boss!")
